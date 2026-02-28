fd1377cd7208e32773c9f57b7e376211c948e7de
aldi <aldinissa093@gmail.com>
2026-03-01 04:00:30 +0700

    dom2

--- main.js
+++ main.js
@@ -13,20 +13,18 @@ card.style.boxShadow = "0 2px 5px 2px rgb(0, 0, 0, 0.2)"
 card.style.padding = "10px"
 card.style.display = "flex"
 card.style.flexDirection = "column"
-card.style.gap = "10px"
   
 const namaProduk = document.createElement('b')
 namaProduk.id = "produk"
 
-const harga = document.createElement('b')
-harga.id = "harga"
-
 const buttonEdit = document.createElement('button')
 buttonEdit.id = "buttonEdit"
+buttonEdit.style.marginTop = "15px"
 buttonEdit.textContent = "edit"
 
 const buttonReset = document.createElement('button')
 buttonReset.id = "buttonReset"
+buttonReset.style.marginTop = "15px"
 buttonReset.textContent= "hapus"
 
 const divv = document.getElementById('carddiv')
@@ -35,26 +33,26 @@ const b2 = document.getElementById('harga')
 const button1 = document.getElementById('buttonEdit')
 const button2 = document.getElementById('buttonReset')
 
-let infoUser = []
+let kartu = card.append(namaProduk, buttonEdit, buttonReset)
+let infoUser = [];
+let total;
 
 form1.addEventListener("submit", function(event){
     event.preventDefault();
 
+    total + 1
+
     const inputProduk = document.getElementById('inputProduk').value
     const inputHarga = document.getElementById('inputHarga').value
     let result;
-    let result2;
 
     if(inputProduk === "" || inputHarga === "") {
         alert1.textContent = "harap di isi"
         alert1.style.color = "red"
     }else if(inputProduk !== "" || inputHarga !== "") {
         alert1.textContent = ""
-        result = "nama barang: " + inputProduk
-        result2 = "harga nya: " + inputHarga
-        infoUser.push(result, result2)
-        card.append(namaProduk, harga, buttonEdit, buttonReset)
-
-        body.append(card)
+        result = "nama barang : " + inputProduk + "<br><br>" + "harga nya : " + inputHarga
+        infoUser.push(result)
+        divResult.append(card)
     }
 })
